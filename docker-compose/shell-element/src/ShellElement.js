22871c550c2860b95a6743a6af870af0e98d236e
Pashovski <pashdoctor@gmail.com>
2023-02-06 22:12:13 -0500

    update ShellElement.js

--- docker-compose/shell-element/src/ShellElement.js
+++ docker-compose/shell-element/src/ShellElement.js
@@ -5,10 +5,14 @@ const logo = new URL(
   import.meta.url
 ).href;
 
+const dryImg = new URL('../assets/dry.jpeg', import.meta.url).href;
+
 export class ShellElement extends LitElement {
   static get properties() {
     return {
       title: { type: String },
+      washerStatus: { type: String },
+      showImage: { type: Boolean },
     };
   }
 
@@ -25,11 +29,15 @@ export class ShellElement extends LitElement {
         max-width: 960px;
         margin: 0 auto;
         text-align: center;
-        background-color: var(--shell-element-background-color);
+        background-color: #f9efea; /*change the background color*/
+      }
+      body {
+        background-color: edede9;
       }
 
       main {
         flex-grow: 1;
+        padding: 30px; /*add some padding */
       }
 
       .logo {
@@ -46,13 +54,18 @@ export class ShellElement extends LitElement {
         }
       }
 
-      .app-footer {
-        font-size: calc(12px + 0.5vmin);
-        align-items: center;
+      h1 {
+        font-size: 2.5em; /*increase the font size of the title */
+        color: #9b8b7a; /*change the title color */
+        text-transform: uppercase; /*change the title to uppercase */
+        letter-spacing: 2px; /*add spacing between letters */
       }
 
-      .app-footer a {
-        margin-left: 5px;
+      h2 {
+        font-size: 1.5em; /*increase the font size of the status */
+        color: #f5a623; /*change the status color to a softer orange */
+        text-transform: capitalize; /*change the status to title case */
+        margin-top: 20px; /*add some margin to the status */
       }
     `;
   }
@@ -60,34 +73,58 @@ export class ShellElement extends LitElement {
   constructor() {
     super();
     this.title = '221 Laundry';
+    this.showImage = false;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    let ws = new WebSocket('ws://192.168.1.230:8000/ws/123');
+    // let ws = new WebSocket("ws://localhost:8000/ws/123");
+
+    ws.onopen = () => {
+      ws.send('Hello, server!');
+    };
+
+    ws.onmessage = event => {
+      let data;
+      if (['idle', 'washing'].includes(event.data)) {
+        data = capitalize(event.data);
+      } else {
+        data = JSON.parse(event.data)[0]['_value'];
+      }
+      this.washerStatus = data;
+      this.showImage = !this.showImage;
+    };
+
+    let reconnect = () => {
+      setTimeout(() => {
+        ws = new WebSocket('ws://192.168.1.230:8000/ws/123');
+        // ws = new WebSocket("ws://localhost:8000/ws/123");
+      }, 5000);
+    };
+    function capitalize(word) {
+      return word[0].toUpperCase() + word.substr(1);
+    }
+    ws.onclose = event => {
+      console.log('WebSocket closed with code: ', event.code);
+      reconnect();
+    };
   }
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
+        ${
+          this.showImage
+            ? html`<div><img alt="Dry Image" src=${dryImg} /></div>`
+            : html`<div class="logo">
+                <img alt="Washing machine Icon" src=${logo} />
+              </div>`
+        }
+        </div>
         <h1>${this.title}</h1>
-
-        <p>Edit <code>src/ShellElement.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        <h2>${this.washerStatus}</h2>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
